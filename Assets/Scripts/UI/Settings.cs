af008a87b45151420fba5ad96f52cb1167a68f81
herald-of-spring <anhnguye@usc.edu>
2025-03-12 18:39:51 -0700

    added gray screen and moved restart and quit buttons

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Settings : MonoBehaviour
 {
     public GameObject menuUI;
+    public GameObject grayOut;
 
     public void ShowMenu() {
         if (menuUI.activeSelf) {
+            grayOut.SetActive(false);
             menuUI.SetActive(false);
             Time.timeScale = 1f;
         } else {
+            grayOut.SetActive(true);
             menuUI.SetActive(true);
             Time.timeScale = 0f;
         }

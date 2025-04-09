05f0bbc3be55214ee32292d80c10f62c381b33e8
XieXintong1210 <84652358+XieXintong1210@users.noreply.github.com>
2025-04-10 04:30:46 +0800

    Add instruction hint on level 2

---
+++ Assets/Scripts/UI/ControlHint.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlHint : MonoBehaviour
+{
+    public GameObject ControlHintObject;  
+    public float duration = 5f;
+    public float shakeAmount = 3f;
+    public float shakeSpeed = 1f;
+    private Vector3 originalPos;
+    private Vector3 shakeDirection;
+    private RectTransform rectTransform;
+
+    public Button buttonToMonitor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = ControlHintObject.gameObject.GetComponent<RectTransform>();
+        originalPos = rectTransform.localPosition;  // initial position
+        shakeDirection = new Vector3(1, 1, 0).normalized;   
+        ControlHintObject.SetActive(false);
+        StartCoroutine(ShakeThenHide()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(buttonToMonitor != null)
+        {
+            buttonToMonitor.onClick.AddListener(DeactivateSelf);
+        }
+        
+    }
+
+
+    System.Collections.IEnumerator ShakeThenHide()
+    {
+
+        // wait for the camera overview
+        yield return new WaitForSeconds(3f);
+
+        ControlHintObject.SetActive(true);
+
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            float shakeOffset = Mathf.Sin(timer * shakeSpeed) * shakeAmount;
+            rectTransform.localPosition = originalPos + shakeDirection * shakeOffset;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        ControlHintObject.SetActive(false); 
+    }
+
+    void DeactivateSelf()
+    {
+        // 将自身设为inactive
+        ControlHintObject.gameObject.SetActive(false);
+    }
+
+}

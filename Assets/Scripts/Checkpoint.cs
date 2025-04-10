873bd1bbc526e4074aed626cc1fea519bf6ed087
Dawnqimiao <1399836977@qq.com>
2025-04-09 19:48:18 -0700

    Make checkpoints become green after the player passed it.

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,23 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class checkpoint : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-        
-//     }
-// }
-
 public class Checkpoint : MonoBehaviour
 {
+
+    private void Start()
+    {
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -40,6 +30,13 @@ public class Checkpoint : MonoBehaviour
 
             // ability num
             CheckpointManager.Instance.SetAbilityCount(other.gameObject.GetComponent<PickupAndPlace>().abilityNum);
+
+            //After passing the checkpoint, change the checkpoint's color to green
+            SpriteRenderer[] childRenderers = GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer renderer in childRenderers)
+            {
+                renderer.color = Color.green;
+            }
         }
     }
 
